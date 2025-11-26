05de4c39bb78dad60db9dad6c49c324c1cc83623
Cecil Mensah <cecmensah@gmail.com>
2025-11-26 00:41:21 +0000

    cleanup

--- webApp/webApp/auth.py
+++ webApp/webApp/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from . import db
 from .model import User
 from .forms import LoginForm, SignupForm
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
